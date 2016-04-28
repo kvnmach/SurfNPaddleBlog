01c6d6932f79e958ef6cdafa4b5a11edd3ea33f9
Kevin <kvnmach@gmail.com>
2016-04-28 16:17:49 -0500

    added properties to blopost

--- SurfNPaddleBlog/Models/BlogPost.cs
+++ SurfNPaddleBlog/Models/BlogPost.cs
@@ -7,5 +7,10 @@ namespace SurfNPaddleBlog.Models
 {
     public class BlogPost
     {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
