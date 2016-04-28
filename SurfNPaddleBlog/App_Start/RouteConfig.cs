adc9704a7c059f7d7124d82b931c6c5737625f29
Kevin <kvnmach@gmail.com>
2016-04-28 17:10:19 -0500

    changed routing

--- SurfNPaddleBlog/App_Start/RouteConfig.cs
+++ SurfNPaddleBlog/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SurfNPaddleBlog
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "BlogPosts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
