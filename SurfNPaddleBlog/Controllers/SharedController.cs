66c5a77f0c08379fb8e7ed824ccd6342b2405cb2
Kevin <kvnmach@gmail.com>
2016-04-28 17:49:45 -0500

    created view

---
+++ SurfNPaddleBlog/Controllers/SharedController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SurfNPaddleBlog.Controllers
+{
+    public class SharedController : Controller
+    {
+        public ActionResult Create()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
